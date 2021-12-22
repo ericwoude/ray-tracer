ed7a2a3a73e4d8707774eedf22e20f826687868b
Eric van der Woude <ericvdwoude@gmail.com>
2021-12-22 14:22:41 +0100

    Added gamma correction

--- include/color.h
+++ include/color.h
@@ -8,9 +8,10 @@
 
 void write_color(std::ostream &out, color pixel_color, int sample_amount)
 {
-    double r = pixel_color.x() / sample_amount;
-    double g = pixel_color.y() / sample_amount;
-    double b = pixel_color.z() / sample_amount;
+    // Divide the color by the sample amount and correct using gamma 2
+    double r = sqrt(pixel_color.x() / sample_amount);
+    double g = sqrt(pixel_color.y() / sample_amount);
+    double b = sqrt(pixel_color.z() / sample_amount);
 
     out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << " "
         << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << " "
