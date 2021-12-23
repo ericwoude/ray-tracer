110be3c6556b3041d99bb5757e19519223100be9
Eric van der Woude <ericvdwoude@gmail.com>
2021-12-23 18:40:56 +0100

    Fixed bug in dielectric material rendering

--- include/material.h
+++ include/material.h
@@ -67,13 +67,13 @@ class dielectric : public material
             rec.front ? (1.0 / refractive_index) : refractive_index;
 
         vec3 u_dir = unit_vector(in.direction());
-        double cos_theta = fmin(dot(-u_dir, rec.n), -1.0);
+        double cos_theta = fmin(dot(-u_dir, rec.n), 1.0);
         double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
 
-        const bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
         vec3 direction;
         if (cannot_refract ||
-            reflectane(cos_theta, refraction_ratio) > random_double())
+            reflectance(cos_theta, refraction_ratio) > random_double())
             direction = reflect(u_dir, rec.n);
         else
             direction = refract(u_dir, rec.n, refraction_ratio);
@@ -81,13 +81,13 @@ class dielectric : public material
         scattered = ray(rec.p, direction);
 
         return true;
-    };
+    }
 
     double refractive_index;
 
    private:
     // Schlick's approximation for reflectance
-    static double reflectane(double cosine, double ref)
+    static double reflectance(double cosine, double ref)
     {
         double r0 = (1 - ref) / (1 + ref);
         r0 *= r0;
